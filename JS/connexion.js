a8477a6781c4adae1ff906749b149274b45aaf34
Yoann <yoyofejean@gmail.com>
2024-05-15 07:17:19 +0200

    #4 Page de connection

---
+++ JS/connexion.js
@@ -0,0 +1,6 @@
+gsap.from('#eye',{scrollTrigger:'#eye', duration:1.5, ease:'bound', y:'-200%'});
+gsap.from('#eyeh1',{scrollTrigger:'#eyeh1', duration:1.5, ease:'bound', y:'-500%'});
+
+//Bouton et italique
+gsap.from('#envoyer',{scrollTrigger:'#envoyer', duration:1, ease:'bound', y:'300%'});
+gsap.from('#compte',{scrollTrigger:'#compte', duration:1, ease:'bound', y:'200%'});
