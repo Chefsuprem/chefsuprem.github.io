6466a46966b869f3d5f44cf7c96e1ea079e0fbad
Chefsuprem <Raphael.vasseur1@outlook.fr>
2024-05-29 02:39:17 +0200

    Fix page contact mails

--- JS/database/mails.js
+++ JS/database/mails.js
@@ -2,44 +2,26 @@
 const contactForm = document.getElementById("contactForm");
 
 contactForm.addEventListener("submit", (event) => {
-
-  event.preventDefault();
-
-  //Se connecter au serveur
-  //Email.server = {
-  //  host: "smtp.elasticemail.com",
-  //  username: "agence-1ris@gmx.com",
-  //  password: "0574B69AD36120C562199C3F90360F5A5C59",
-  //  secure: true,
-  //};
   
-  //Envoyer le mail
-  Email.send({
-    SecureToken : "b025c27b-85dd-43f3-b839-1f4c9f2a4f2b",
-    To : `agence-1ris@gmx.com`,
-    From : `${contactForm["email"].value}`,
-    Subject : "Test mails contacts",
-    Body : `${contactForm["description"].value}`
+  event.preventDefault();
+  
+  let config = {
+    from_name: `${contactForm["nom"].value} ${contactForm["prenom"].value}`,
+    message: contactForm["description"].value,
+    reply_to: contactForm["email"].value
+  }
 
-  }).then((message) => {
+  emailjs.send("service_9y0odtt", "template_7znbnds", config)
+  .then(() => {
 
-    alert(message)
+    //alert("C'est passé");
+    
+  })
+  .catch((error) => {
+
+    console.log(error.code);
+    console.log(error.message);
+
   })
 
-  //Email.send({
-  //  Host : "smtp.elasticemail.com",
-  //  Username : "agence-1ris@gmx.com",
-  //  Password : "0574B69AD36120C562199C3F90360F5A5C59",
-  //  To : 'agence-1ris@gmx.com',
-  //  From : `agence-1ris@gmx.com`,
-  //  Subject : "This is the subject",
-  //  Body : `${contactForm["description"].value}`
-  //}).then(
-  //  message => alert(message)
-  //);
-  
 })
-
-//agence-1ris@gmx.com
-//0574B69AD36120C562199C3F90360F5A5C59
-//smtp.elasticemail.com 2525
